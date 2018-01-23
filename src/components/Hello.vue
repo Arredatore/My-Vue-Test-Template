8fddfcbf80212fdd1840945d0440e9ba9a24b723
Arredatore <sculpterra@gmail.com>
2018-01-23 16:10:17 +0300

    Initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,100 @@
+<template>
+	<div class="hello">
+		<h1>{{ msg }}</h1>
+		<h2>Essential Links</h2>
+		<ul>
+			<li>
+				<a href="https://vuejs.org" target="_blank">Core Docs</a>
+			</li>
+			<li>
+				<a href="https://forum.vuejs.org" target="_blank">Forum</a>
+			</li>
+			<li>
+				<a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a>
+			</li>
+			<li>
+				<a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
+			</li>
+			<br>
+			<li>
+				<a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
+			</li>
+		</ul>
+		<h2>Ecosystem</h2>
+		<ul>
+			<li>
+				<a href="http://router.vuejs.org/" target="_blank">vue-router</a>
+			</li>
+			<li>
+				<a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
+			</li>
+			<li>
+				<a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
+			</li>
+			<li>
+				<a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
+			</li>
+		</ul>
+		<p>hfjhhfnmn</p>
+		<div class="text-xs-center">
+    <v-badge left>
+      <span slot="badge">6</span>
+      <v-icon large color="grey lighten-1">shopping_cart</v-icon>
+    </v-badge>
+
+    <v-badge color="red">
+      <span slot="badge">!</span>
+      <v-icon large color="grey">mail</v-icon>
+    </v-badge>
+  </div>
+	<v-form v-model="valid">
+    <v-text-field
+      label="Name"
+      v-model="name"
+      :rules="nameRules"
+      :counter="10"
+      required
+    ></v-text-field>
+    <v-text-field
+      label="E-mail"
+      v-model="email"
+      :rules="emailRules"
+      required
+    ></v-text-field>
+  </v-form>
+	</div>
+</template>
+
+<script>
+	export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App test'
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	h1,
+	h2 {
+		font-weight: normal;
+	}
+
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+
+	li {
+		display: inline-block;
+		margin: 0 10px;
+	}
+
+	a {
+		color: #42b983;
+	}
+</style>
